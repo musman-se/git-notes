c29edf6022286aff6bcf25e806e9de6c663ef92f
Muhammad Usman <m.usman@emumba.com>
2024-12-07 17:01:39 +0500

    [feature/list-view-ui] add PublicGist component with layout toggle and ListView integration

---
+++ src/components/ListView/ListView.tsx
@@ -0,0 +1,17 @@
+// React Dependencies
+import React from 'react'
+
+// External Dependencies
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css"; 
+import "ag-grid-community/styles/ag-theme-quartz.css";
+
+const ListView = () => {
+    return (
+        <div>
+            <p>List View</p>
+        </div>
+    )
+}
+
+export default ListView
