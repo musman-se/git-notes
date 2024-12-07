57866fabbab544a547e0eafe5e43345bda38503b
Muhammad Usman <m.usman@emumba.com>
2024-12-07 16:38:52 +0500

    [feature/list-view-ui] refactor: update Input component to use React components for icons and adjust Navbar to utilize SVG imports

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,18 +2,19 @@
 import React from 'react'
 
 type InputProps = {
-    icon?: string, // svg, png, etc
+    Icon?: React.ElementType,
+    iconClassName?: string,
     placeholder?: string,
     value: string,
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input = ({ icon, placeholder, value, onChange }: InputProps) => {
+const Input = ({ Icon, iconClassName, placeholder, value, onChange }: InputProps) => {
     return (
         <div className='flex relative'>
-            {icon && <img src={icon} className="absolute top-1/4 px-2" alt="Vite logo" />}
+            { Icon && <Icon className={`absolute top-1/4 left-2 text-white ${iconClassName}`} /> }
             <input 
-                className={`p-2.5 rounded-[4px] text-sm border border-white/50 bg-transparent text-white focus:outline-none focus:border-white ${icon ? 'pl-8' : ''}`}
+                className={`p-2.5 rounded-[4px] text-sm border border-white/50 bg-transparent text-white focus:outline-none focus:border-white ${Icon ? 'pl-8' : ''}`}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
