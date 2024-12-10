fdcb2686da13cb36b6db0937e582d8ba645bf02d
Muhammad Usman <m.usman@emumba.com>
2024-12-10 12:07:30 +0500

    [feature/gist-card] added custom colors based on the figma design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGreen: '#003B44',
+        darkGray: '#3D3D3D',
+        lightGray: '#EFEFEF',
+        lighterGray: '#E3E3E3',
+        mediumGray: '#B7B7B7',
+        gray: '#A3A3A3',
+        green: '#059411',
+        blue: '#0072D1',
+        darkSilver: '#7A7A7A',
+        skyBlue: '#00A3FF', 
+      }
+    },
   },
   plugins: [],
 }
